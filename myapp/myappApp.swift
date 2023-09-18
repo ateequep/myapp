24bc44a79676f00e61da6874d4bcb021fd0fa08c
ateequep <ateeque.patel@gmail.com>
2023-09-18 14:17:50 -0700

    Initial Commit

---
+++ myapp/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  myapp
+//
+//  Created by Ateeque Patel on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
